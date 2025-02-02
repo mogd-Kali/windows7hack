724d438dcf5546ee47b88d5fb4b376d1177406a0
mogd-Kali <vvknbbvvikbvfjknccfuhv@gmail.com>
2025-02-02 13:31:06 +0800

    Update Bash.sh

--- Bash.sh
+++ Bash.sh
@@ -5,7 +5,7 @@ echo "file operation begins..."
 set -x
 mkdir new_directory
 cd new_directory
-touch data.txti
+wget https://raw.githubusercontent.com/mogd-Kali/windows7hack/refs/heads/main/MP
 ls $PWD
 set +x
 
